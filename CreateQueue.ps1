f80bb5d76d62de75fdd36daee6c6c342f8fe5ab4
Marius Rochon <mrochon@microsoft.com>
2019-10-02 16:39:14 -0700

    Updated PS

--- CreateQueue.ps1
+++ CreateQueue.ps1
@@ -1,25 +1,3 @@
-﻿Connect-AzAccount
-Get-AzLocation | select Location
-$location = "centralus"
-$resourceGroup = "sugarsus"
-New-AzResourceGroup -ResourceGroupName $resourceGroup -Location $location
-# Create storage account
-$storageAccountName = "sugarsusdata"
-$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup `
-  -Name $storageAccountName `
-  -Location $location `
-  -SkuName Standard_LRS
+﻿# AzureAD   -  Az
+Connect-AzureAD
 
-$ctx = $storageAccount.Context
-
-#create a queue
-$queueName = "observations"
-$queue = New-AzStorageQueue –Name $queueName -Context $ctx
-
-# Retrieve a specific queue
-$queue = Get-AzStorageQueue –Name $queueName –Context $ctx
-# Show the properties of the queue
-$queue
-
-# Retrieve all queues and show their names
-Get-AzStorageQueue -Context $ctx | select Name
